9673cd16c31e80f231f30bc49b1a4f0f500171e9
mancode23 <shrijiman2371@gmail.com>
2023-06-11 16:38:01 +0530

    Some O/P line added

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Manav Welcome");
         System.out.println("Kya hal chal apke");
+        System.out.println("Abe bol na bhai kya bol raha tha tu ??");
         
     }
 }
