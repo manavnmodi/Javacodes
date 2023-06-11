cdc5d07f94c9ae6f7c2d1dfc9d4cfcc5388b9213
mancode23 <shrijiman2371@gmail.com>
2023-06-11 16:31:29 +0530

    First code

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Manav Welcome");
+        System.out.println("Kya hal chal ");
+        
+    }
+}
